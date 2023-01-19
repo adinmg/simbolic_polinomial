8cb6eea180df31ac0726a0cc327981ed35654ece
adinmg <adinmg@fisica.ugto.mx>
2023-01-19 17:12:02 -0600

    UniPoly version 3

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 
 class UniPoly:
     # Unipoly creates univariate polynomials with integer coefficients
-    # Version 1
+    # Version 3
 
-    def __init__(self, coefficient, indeterminate, exponent):
+    def __init__(self, coefficient=1, indeterminate='x', exponent=0):
         # Create a UniPoly monomial.
+
+        # Create a UniPoly object from an integer coefficient, string indeterminate, and 
+        # non-negative integer exponent
 
         # Validate that the coefficients is an int.
         if not isinstance(coefficient, int):
@@ -19,14 +22,40 @@ class UniPoly:
         if (not isinstance(indeterminate, str) or len(indeterminate) != 1 or not indeterminate[0].isalpha()):
             raise ValueError("The indeterminate for a UniPoly must be an alphabetic string of length 1.")
         
-        self.coefficient = coefficient
+        # The 'variable' for the polynomial.
         self.indeterminate = indeterminate
-        self.exponent = exponent
+
+        # The terms in the polynomial. Each key is an int exponent and each value is the int coefficient.
+        if coefficient != 0:
+            self.terms = {exponent: coefficient}
+        else:
+            self.terms = dict()
     
+    def __neg__(self):
+        # Negate the polynomial term-wise unless its is already 0.
+        if not self.terms:
+            return self
+        
+        new_poly = UniPoly(0, self.indeterminate)
+        for exponent in self.terms:
+            new_poly.terms[exponent] = -self.terms[exponent]
+
+        return new_poly
+
     def __repr__(self):
         # Create the object representation of the polynomial
-        return f"UniPoly({self.coefficient}, " + f"{repr(self.indeterminate)}, {self.exponent})"
+        return str(self)
 
     def __str__(self):
-        # Create the displayable string form of the polynomial
-        return f"{self.coefficient}*" + f"{self.indeterminate}**{self.exponent}"
+        # Do a very rough and incomplete conversion to str.
+
+        if not self.terms:
+            return '0'
+        
+        print_terms = [
+                f"{self.terms[exponent]}*{self.indeterminate}**{exponent}"
+                for exponent in sorted(self.terms, reverse=True)
+                ]
+        
+        return " + ".join(print_terms)
+
